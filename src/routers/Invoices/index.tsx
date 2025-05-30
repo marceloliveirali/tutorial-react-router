c4ab0f6ebc4e53d2d8489a96a2a7d175c2088ba4
Marcelo de Oliveira Lima <marceloliveirali@gmail.com>
2025-05-30 17:34:42 -0300

    Listing invoices

--- src/routers/Invoices/index.tsx
+++ src/routers/Invoices/index.tsx
@@ -1,10 +1,31 @@
+import { Link } from "react-router-dom";
+import { getInvoices } from "../../data";
+
 function Invoices()
 {
+    const invoices = getInvoices();
+
     return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Invoices</h2>
-        </main>
+        <>
+            <div style={{ display: "flex" }}>
+                <nav style={{
+                    borderRight: "solid 1px",
+                    padding: "1rem",
+                    }}
+                >
+                    {invoices.map((invoice) => (
+                        <Link
+                            to={`/invoices/${invoice.number}`}
+                            style={{ display: "block", margin: "1rem 0" }}
+                            key={invoice.number}
+                        >
+                            {invoice.name}
+                        </Link>
+                    ))}
+                </nav>
+            </div>
+        </>
     );
 }
 
-export default Invoices
+export default Invoices;
