821412ab61d20c0dd5e59f3d3553ef381b674b2b
Marcelo de Oliveira Lima <marceloliveirali@gmail.com>
2025-05-30 21:24:43 -0300

    No match route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Expenses from './routers/Expenses/index.tsx'
 import Invoices from './routers/Invoices/index.tsx'
+import NotFound from './routers/NotFound/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,9 +12,10 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/expenses" element={<Expenses />} />
-            <Route path="/invoices" element={<Invoices />} />
+          <Route path="/invoices" element={<Invoices />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>    
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
---
+++ src/routers/NotFound/index.tsx
@@ -0,0 +1,10 @@
+function NotFound()
+{
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <p>Não encontrado!</p>
+        </main>
+    )
+}
+
+export default NotFound;
